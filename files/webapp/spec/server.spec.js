6df657ddef6b26b1a936ef79a8b7274e92398189
Fidel Makatia <fidelmakatia@gmail.com>
2023-03-21 17:36:05 +0300

    with unit test

--- files/webapp/spec/server.spec.js
+++ files/webapp/spec/server.spec.js
@@ -10,8 +10,16 @@ describe('calc', ()=> {
 describe('get messeges',()=>{
     it('should return 200 ok', ()=>{
         request.get('http://localhost:3000/messeges', (err,res)=>{
-            console.log(res)
+            expect(res.statusCode).toEqual(200)
+            done()
+        })
+    })
+
+    it('should return a list thats not empty', (done)=>{
+        request.get('http://localhost:3000/messeges', (err,res)=>{
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0)
             done()
         })
     })
 })
+
